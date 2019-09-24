f5002be6580a5b17af0f688bb017d30600481662
donbur1 <donceburz@gmail.com>
2019-09-24 22:05:09 +0300

    Fixed DB function, need to fix authentication issues

--- Server/src/main/java/Server/DB.java
+++ Server/src/main/java/Server/DB.java
@@ -1,13 +1,22 @@
 package Server;
 
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoClient;
 
 public class DB {
-    private static MongoClientURI mongoClient;
+    private static com.mongodb.DB mongoDB;
 
-    synchronized public static MongoClientURI Instance(){
-        if(mongoClient == null)
-            mongoClient = new MongoClientURI("mongodb://durakdb:durakdbopp@durakdb-b1ola.azure.mongodb.net/test?retryWrites=true&w=majority");
-        return  mongoClient;
+    synchronized public static com.mongodb.DB Instance(){
+        if(mongoDB == null){
+            MongoClientURI mongoClientURI = new MongoClientURI(
+                    "mongodb://durak:durak@durakdb-shard-00-00-b1ola.azure.mongodb.net:27017,durakdb-shard-00-01-b1ola.azure.mongodb.net:27017,durakdb-shard-00-02-b1ola.azure.mongodb.net:27017/test?ssl=true&replicaSet=DurakDB-shard-0&authSource=admin&retryWrites=true&w=majority");
+            try{
+                MongoClient mongoClient = new MongoClient(mongoClientURI);
+                mongoDB = mongoClient.getDB("test");
+            } catch (java.net.UnknownHostException ex){
+
+            }
+        }
+        return  mongoDB;
     }
 }
