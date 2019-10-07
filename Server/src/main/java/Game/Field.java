5e11852558fa7dab620cb5f8279d070c2d0a6b4d
donbur1 <donceburz@gmail.com>
2019-10-07 20:54:02 +0300

    Atackes were not added to pair list

--- Server/src/main/java/Game/Field.java
+++ Server/src/main/java/Game/Field.java
@@ -35,6 +35,7 @@ public class Field {
     public void attack(Card a) {
         if (canAttack() && isValidAttack(a)) {
             Pair newAttackPair = new Pair(a);
+            pairs.add(newAttackPair);
         } else {
             throw new IllegalArgumentException("You can't attack.");
         }
