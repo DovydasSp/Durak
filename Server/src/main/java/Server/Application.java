aedd23ec27bf600a965e7b5e0e6e1e9fe6757a68
Dovydas <dovydas.speckauskas@ktu.edu>
2019-10-08 18:16:20 +0300

    Server
    Updated server IP to work with azure

--- Server/src/main/java/Server/Application.java
+++ Server/src/main/java/Server/Application.java
@@ -5,15 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-import java.util.Collections;
-
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application{
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections
-                .singletonMap("server.address", "192.168.0.106"));
-                //.singletonMap("server.port", "6969"));
-        app.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
