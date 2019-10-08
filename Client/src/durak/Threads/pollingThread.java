f061846494749a1f4015653dc55f112f62e5e564
Dovydas <dovydas.speckauskas@ktu.edu>
2019-10-08 18:41:02 +0300

    Client
    Updated polling times, so it wouldnt bug the server.

--- Client/src/durak/Threads/pollingThread.java
+++ Client/src/durak/Threads/pollingThread.java
@@ -23,7 +23,7 @@ public class pollingThread extends Observable implements Runnable {
         { 
             boolean run = true;
             while(run){
-                Thread.sleep(100);
+                Thread.sleep(1000);
                 GameData gd = connection.poll(gameData);
                 if(gd != null){
                     gameData = gd;
